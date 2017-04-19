7bfcb1911979b2e9eb75c497824dc43c9a761a6a
Praveen <larchpraveen22@outlook.com>
2017-04-19 17:44:24 +0100

    Update dashboard.js

--- res/js/dashboard.js
+++ res/js/dashboard.js
@@ -14,7 +14,7 @@ var ctx = document.getElementById("energyGen");
 var energyGen = new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ["Nuclear", "Solar", "Hydro", "Pavegen", "Other"],
+        labels: ["Nuclear", "Solar", "Hydro", "Pavement Pressure Generation", "Other"],
         datasets: [{
             label: '% Generation',
             data: [nuclear, solar, hydro, pavegen, other],
